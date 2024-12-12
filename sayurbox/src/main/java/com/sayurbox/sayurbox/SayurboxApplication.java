1059775c12f0076435da30cb3f0766433d0ba2c6
derukhdj22@gmail.com <derukhdj22@gmail.com>
2024-12-13 03:09:30 +0700

    Push

---
+++ sayurbox/src/main/java/com/sayurbox/sayurbox/SayurboxApplication.java
@@ -0,0 +1,13 @@
+package com.sayurbox.sayurbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayurboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayurboxApplication.class, args);
+	}
+
+}
---
+++ sayurbox/src/test/java/com/sayurbox/sayurbox/SayurboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayurbox.sayurbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayurboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
